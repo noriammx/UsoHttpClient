1888b60e97c759828cde2a3c2f800a8d0ce5563e
Luis Martinez <marino.martinez@outlook.com>
2019-01-10 12:43:28 -0600

    Implementación, validación de componente que permite cargar más de un archivo utilizando la clase HTTPCliente, así como un servidor remoto.

---
+++ HTTPClient/UploadFilesToRemoteServer/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace SubirEvidencias
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNombreArchivo = new System.Windows.Forms.TextBox();
+            this.btnUpLoadOneFile = new System.Windows.Forms.Button();
+            this.btnMultipleFiles = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnCargarMultiples = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Seleccionar Archivo:";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNombreArchivo
+            // 
+            this.txtNombreArchivo.Location = new System.Drawing.Point(167, 15);
+            this.txtNombreArchivo.Name = "txtNombreArchivo";
+            this.txtNombreArchivo.Size = new System.Drawing.Size(351, 20);
+            this.txtNombreArchivo.TabIndex = 1;
+            // 
+            // btnUpLoadOneFile
+            // 
+            this.btnUpLoadOneFile.Location = new System.Drawing.Point(369, 41);
+            this.btnUpLoadOneFile.Name = "btnUpLoadOneFile";
+            this.btnUpLoadOneFile.Size = new System.Drawing.Size(149, 23);
+            this.btnUpLoadOneFile.TabIndex = 0;
+            this.btnUpLoadOneFile.Text = "Cargar";
+            this.btnUpLoadOneFile.UseVisualStyleBackColor = true;
+            this.btnUpLoadOneFile.Click += new System.EventHandler(this.btnUpLoadOneFile_Click);
+            // 
+            // btnMultipleFiles
+            // 
+            this.btnMultipleFiles.Location = new System.Drawing.Point(12, 91);
+            this.btnMultipleFiles.Name = "btnMultipleFiles";
+            this.btnMultipleFiles.Size = new System.Drawing.Size(149, 23);
+            this.btnMultipleFiles.TabIndex = 0;
+            this.btnMultipleFiles.Text = "Seleccionar Archivos:";
+            this.btnMultipleFiles.UseVisualStyleBackColor = true;
+            this.btnMultipleFiles.Click += new System.EventHandler(this.btnMultipleFiles_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(167, 91);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(351, 95);
+            this.listBox1.TabIndex = 2;
+            // 
+            // btnCargarMultiples
+            // 
+            this.btnCargarMultiples.Location = new System.Drawing.Point(369, 192);
+            this.btnCargarMultiples.Name = "btnCargarMultiples";
+            this.btnCargarMultiples.Size = new System.Drawing.Size(149, 23);
+            this.btnCargarMultiples.TabIndex = 0;
+            this.btnCargarMultiples.Text = "Cargar";
+            this.btnCargarMultiples.UseVisualStyleBackColor = true;
+            this.btnCargarMultiples.Click += new System.EventHandler(this.btnCargarMultiples_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 237);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtNombreArchivo);
+            this.Controls.Add(this.btnCargarMultiples);
+            this.Controls.Add(this.btnUpLoadOneFile);
+            this.Controls.Add(this.btnMultipleFiles);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Ejemplo de Uso de HTTPClient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNombreArchivo;
+        private System.Windows.Forms.Button btnUpLoadOneFile;
+        private System.Windows.Forms.Button btnMultipleFiles;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnCargarMultiples;
+    }
+}
+
---
+++ HTTPClient/UploadFilesToRemoteServer/Form1.cs
@@ -0,0 +1,186 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SubirEvidencias
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog files;
+        List<String> archivos;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtNombreArchivo.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private async void btnUpLoadOneFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1 == null)
+            {
+                MessageBox.Show("No se ha seleccionado ningún archivo");
+                return;
+            }
+
+
+            if (openFileDialog1.FileName == null || openFileDialog1.FileName == string.Empty || txtNombreArchivo.Text == string.Empty)
+            {
+                MessageBox.Show("No se ha seleccionado ningún archivo");
+                return;
+            }
+
+            //Definimos los parámetros que se enviarian al servicio.
+            //Archivo, se tiene que convertir a StreamContent
+            HttpContent fileStreamContent = new StreamContent(System.IO.File.Open(openFileDialog1.FileName, FileMode.Open, FileAccess.Read));
+            //Parámetros adicionales
+            HttpContent stringContent = new StringContent("NOM-ALTALT18-003");
+            //Creamos el cliente que se va a usar
+            using (var client = new HttpClient())
+            {
+                //Definimos la dirección base para los servicios
+                client.BaseAddress = new Uri("http://localhost:53587/");
+                //Definimos el tipo de contenido MultiparForm, para poder subir archivos
+                var content = new MultipartFormDataContent();
+                //Se agregan los parámetros que recibe el servicio
+                content.Add(fileStreamContent, openFileDialog1.SafeFileName, openFileDialog1.SafeFileName);
+                content.Add(stringContent, "folioNominacion");
+                //Ejecutamos la llamada del servicio, indicando la acción que se debe ejecutar
+                Cursor.Current = Cursors.WaitCursor;
+                try
+                {
+                    var response = await client.PostAsync("BibliotecaService/GuardarPlantillaPropuestaFromWF", content);
+
+
+                    //Con esta linea se lee la respuesta del resultado
+                    var contents = await response.Content.ReadAsStringAsync();
+
+                    Cursor.Current = Cursors.Default;
+                    //Evaluamos  la repuesta del servicio
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        //return null;
+                        //Evaluar lo que se se desea hacer en caso de que la llamada de servicio falle
+                        MessageBox.Show("El servicio ha fallado, favor de intentarlo más tarde");
+                    }
+                    // return await response.Content.ReadAsStreamAsync();
+                    openFileDialog1 = new OpenFileDialog();
+                    txtNombreArchivo.Text = "";
+                    MessageBox.Show("Servicio Ejecutado Satisfactoriamente" + "\n" + "Respuesta Obtenida: " + contents);
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("El servicio ha fallado, favor de intentarlo más tarde");
+                }
+            }
+
+        }
+
+        private void btnMultipleFiles_Click(object sender, EventArgs e)
+        {
+            files = new OpenFileDialog();
+            files.Multiselect = true;
+            archivos = new List<string>();
+
+            if (files.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                files.FileNames.ToList().ForEach(file =>
+                {
+                    archivos.Add(file);
+
+                });
+
+                listBox1.DataSource = archivos;
+
+            }
+        }
+
+        private async void btnCargarMultiples_Click(object sender, EventArgs e)
+        {
+            if (files == null)
+            {
+                MessageBox.Show("No se ha seleccionado ningún archivo");
+                return;
+            }
+
+
+            if (files.FileName == null || files.FileName == string.Empty || archivos == null || archivos.Count ==0)
+            {
+                MessageBox.Show("No se ha seleccionado ningún archivo");
+                return;
+            }
+
+            //Definimos el tipo de contenido MultiparForm, para poder subir archivos
+            //Definimos los parámetros que se enviarian al servicio.
+            var content = new MultipartFormDataContent();
+
+            for (int i = 0; i < files.FileNames.Length; i++)
+            {
+                //Archivo, se tiene que convertir a StreamContent
+                HttpContent fileStreamContent = new StreamContent(File.Open(files.FileNames[i], FileMode.Open, FileAccess.Read));
+                //Se agregan los parámetros que recibe el servicio
+                content.Add(fileStreamContent, files.SafeFileNames[i], files.SafeFileNames[i]);
+            }          
+            //Parámetros adicionales
+            HttpContent stringContent = new StringContent("NOM-ALTALT18-003");
+            content.Add(stringContent, "folioNominacion");
+
+
+
+            //Creamos el cliente que se va a usar
+            using (var client = new HttpClient())
+            {
+                //Definimos la dirección base para los servicios
+                client.BaseAddress = new Uri("http://localhost:53587/");
+                //Ejecutamos la llamada del servicio, indicando la acción que se debe ejecutar
+                Cursor.Current = Cursors.WaitCursor;
+                try
+                {
+                    var response = await client.PostAsync("BibliotecaService/GuardarEvidencias", content);
+
+                    //Con esta linea se lee la respuesta del resultado
+                    var contents = await response.Content.ReadAsStringAsync();
+
+                    Cursor.Current = Cursors.Default;
+                    //Evaluamos  la repuesta del servicio
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        //return null;
+                        //Evaluar lo que se se desea hacer en caso de que la llamada de servicio falle
+                        MessageBox.Show("El servicio ha fallado, favor de intentarlo más tarde");
+                    }
+                    // return await response.Content.ReadAsStreamAsync();
+                    files = new OpenFileDialog();
+                    listBox1.DataSource = null;
+                    archivos = new List<string>();
+                    MessageBox.Show("Servicio Ejecutado Satisfactoriamente" + "\n" + "Respuesta Obtenida: " + contents);
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("El servicio ha fallado, favor de intentarlo más tarde");
+                }
+            }
+        }
+    }
+}
